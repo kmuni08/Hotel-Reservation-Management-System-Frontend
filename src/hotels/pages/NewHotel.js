4e1420b3cdc441e772a39f9205acf3d18849ab44
kmuni08 <Karishma.Muni24@gmail.com>
2020-11-30 19:52:36 -0500

    maps works

--- src/hotels/pages/NewHotel.js
+++ src/hotels/pages/NewHotel.js
@@ -1,9 +1,99 @@
+import React, { useCallback, useReducer } from 'react';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import './NewHotel.css';
+import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from "../../shared/util/validators";
 
-
-import React from 'react';
+const formReducer = (state, action) => {
+    switch (action.type) {
+        case 'INPUT_CHANGE':
+            let formIsValid = true;
+            for (const inputId in state.inputs) {
+                if (inputId === action.inputId) {
+                    formIsValid = formIsValid && action.isValid;
+                } else {
+                    formIsValid = formIsValid && state.inputs[inputId].isValid;
+                }
+            }
+            return {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [action.inputId] : {
+                        value: action.value,
+                        isValid: action.isValid
+                    }
+                },
+                isValid: formIsValid
+            };
+        default:
+            return state;
+    }
+};
 
 const NewHotel = () => {
-    return <h2>NewHotel Works!</h2>
+
+    const [formState, dispatch] = useReducer(formReducer, {
+        inputs: {
+            name: {
+                value: '',
+                isValid: false
+            },
+            description: {
+                value: '',
+                isValid: false
+            }
+        },
+        isValid: false
+    });
+
+    const inputHandler = useCallback((id, value, isValid) => {
+        dispatch({
+            type: 'INPUT_CHANGE',
+            value: value,
+            isValid: isValid,
+            inputId: id});
+    }, []);
+
+    //want to send this to the backend.
+    const hotelSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    }
+
+    return (
+        <form className = "hotel-form" onSubmit={hotelSubmitHandler}>
+            <Input
+                id = "name"
+                element= "input"
+                type = "text"
+                label = "Name"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid name."
+                onInput = {inputHandler}
+            />
+            <Input
+                id = "address"
+                element= "input"
+                label = "Address"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid address."
+                onInput = {inputHandler}
+            />
+            <Input
+                id = "description"
+                element= "textarea"
+                label = "Description"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+                errorText="Please enter a valid description (at least 5 characters)."
+                onInput = {inputHandler}
+            />
+
+            <Button type="submit" disabled={!formState.isValid}>
+                ADD HOTEL
+            </Button>
+        </form>
+    )
 };
 
 export default NewHotel;
