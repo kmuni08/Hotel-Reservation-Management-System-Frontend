62745201dc88312d0a2069547502771841c8f6f0
kmuni08 <Karishma.Muni24@gmail.com>
2020-12-02 22:09:24 -0500

    Delete in frontend works

--- src/hotels/components/HotelItem.js
+++ src/hotels/components/HotelItem.js
@@ -10,10 +10,25 @@ import './HotelItem.css';
 
 const HotelItem = props => {
     const [showMap, setShowMap] = useState(false);
+    const [showConfirmModal, setShowConfirmModal] = useState(false);
 
     const openMapHandler = () => setShowMap(true);
 
     const closeMapHandler = () => setShowMap(false);
+    const showDeleteWarningHandler = () => {
+        setShowConfirmModal(true);
+    };
+
+    const cancelDeleteHandler = () => {
+        setShowConfirmModal(false);
+    };
+
+    const confirmDeleteHandler = () => {
+        setShowConfirmModal(false);
+        console.log('DELETING...');
+    };
+
+
     return (
         <React.Fragment>
             <Modal
@@ -27,6 +42,21 @@ const HotelItem = props => {
                 <div className = "map_container">
                     <Map center={props.coordinates} zoom={16} />
                 </div>
+            </Modal>
+            <Modal
+                show={showConfirmModal}
+                onCancel = {cancelDeleteHandler}
+                header = "Are you sure?"
+                footerClass = "hotel-item__modal-action"
+                footer = {
+                <React.Fragment>
+                    <Button inverse onClick = {cancelDeleteHandler}> CANCEL </Button>
+                    <Button danger onClick = {confirmDeleteHandler}> DELETE </Button>
+                </React.Fragment>
+            }>
+                <p>
+                    Do you want to proceed and delete this place? It cannot be undone.
+                </p>
             </Modal>
             <li className="hotel-item">
                 <Card className="hotel-item__content">
@@ -50,7 +80,7 @@ const HotelItem = props => {
                     <div className = "hotel_item__actions">
                         <Button inverse onClick={openMapHandler}>VIEW ON MAP</Button>
                         <Button to={`/hotels/${props.id}`} >EDIT</Button>
-                        <Button danger>DELETE</Button>
+                        <Button danger onClick = {showDeleteWarningHandler}>DELETE</Button>
                     </div>
                 </Card>
             </li>
