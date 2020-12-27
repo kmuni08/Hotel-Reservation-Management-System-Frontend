50c19eec6f0e2da08706ac3ab6986313db42eb3e
kmuni08 <Karishma.Muni24@gmail.com>
2020-12-26 19:15:34 -0500

    updated the add new hotel component

--- src/allHotels/pages/AllHotels.js
+++ src/allHotels/pages/AllHotels.js
@@ -7,7 +7,6 @@ const HOTELS = [
         id: '1',
         image: 'https://content.fortune.com/wp-content/uploads/2020/05/F500-2020-338-Hilton-.jpg',
         name: 'Hilton',
-        rating: 4.0,
         address: '42 Street, Midtown NYC',
         location: {
             lat: 40.7484405,
@@ -31,7 +30,6 @@ const HOTELS = [
         id: '2',
         image: 'https://www.gannett-cdn.com/presto/2019/04/16/USAT/15d11370-b0e6-4743-adf0-387d1fa95ab5-AP_Marriott_Starwood_Sale.JPG?crop=4851,2740,x0,y0&width=3200&height=1808&format=pjpg&auto=webp',
         name: 'Marriot',
-        rating: 3.8,
         address: '1 Union Turnpike, Queens',
         location: {
             lat: 30.7484405,
--- src/hotels/pages/NewHotel.js
+++ src/hotels/pages/NewHotel.js
@@ -2,11 +2,15 @@ import React from 'react';
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import './HotelForm.css';
-import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from "../../shared/util/validators";
+import {VALIDATOR_MIN, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 
 const NewHotel = () => {
     const [formState, inputHandler] = useForm({
+        image: {
+            value: '',
+            isValid: false
+        },
         name: {
             value: '',
             isValid: false
@@ -29,6 +33,15 @@ const NewHotel = () => {
 
     return (
         <form className = "hotel-form" onSubmit={hotelSubmitHandler}>
+            <Input
+                id = "image"
+                element= "input"
+                type = "text"
+                label = "Image"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid url"
+                onInput = {inputHandler}
+            />
             <Input
                 id = "name"
                 element= "input"
@@ -53,6 +66,60 @@ const NewHotel = () => {
                 label = "Description"
                 validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
                 errorText="Please enter a valid description (at least 5 characters)."
+                onInput = {inputHandler}
+            />
+            <Input
+                id = "deluxeRooms"
+                element= "input"
+                type = "text"
+                label = "Deluxe Rooms Available"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MIN(5)]}
+                errorText="Please enter a valid number of rooms."
+                onInput = {inputHandler}
+            />
+            <Input
+                id = "deluxePrice"
+                element= "input"
+                type = "text"
+                label = "Deluxe Price Per Night"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MIN(50)]}
+                errorText="Please enter a valid number for price."
+                onInput = {inputHandler}
+            />
+            <Input
+                id = "standardRooms"
+                element= "input"
+                type = "text"
+                label = "Standard Rooms Available"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MIN(5)]}
+                errorText="Please enter a valid number of rooms."
+                onInput = {inputHandler}
+            />
+            <Input
+                id = "standardPrice"
+                element= "input"
+                type = "text"
+                label = "Standard Room Price Per Night"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MIN(50)]}
+                errorText="Please enter a valid number for price."
+                onInput = {inputHandler}
+            />
+            <Input
+                id = "suiteRooms"
+                element= "input"
+                type = "text"
+                label = "Suite Rooms Available"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MIN(5)]}
+                errorText="Please enter a valid number of rooms."
+                onInput = {inputHandler}
+            />
+            <Input
+                id = "suitePrice"
+                element= "input"
+                type = "text"
+                label = "Suite Price Per Night"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MIN(50)]}
+                errorText="Please enter a valid number for price."
                 onInput = {inputHandler}
             />
 
