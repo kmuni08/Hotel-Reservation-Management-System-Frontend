c375721d07e420d6f604ce5562739b096003f4d0
kmuni08 <Karishma.Muni24@gmail.com>
2021-01-05 13:03:15 -0500

    minor changes to nav link

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -21,7 +21,7 @@ const NavLinks = props => {
             )}
             {!auth.isLoggedIn && (
                 <li>
-                    <NavLink to="/auth"> AUTHENTICATE</NavLink>
+                    <NavLink to="/auth"> LOGIN</NavLink>
                 </li>
             )}
             {auth.isLoggedIn && (auth.userId === "5ff23a8f60bb5e0017b1a8d6" || auth.userId === "5ff283b6372d6700171b2cd9" || auth.userId === "5ff283f4372d6700171b2cda" || auth.userId === "5ff2840f372d6700171b2cdb") && (
@@ -47,47 +47,6 @@ const NavLinks = props => {
 
         </ul>
 
-    // <ul className = "nav-links">
-    //     {/*<li>*/}
-    //     {/*    <NavLink to="/"> WELCOME PAGE</NavLink>*/}
-    //     {/*</li>*/}
-    //     {(auth.isLoggedIn || !auth.isLoggedIn) && auth.userId !== "5ff23a8f60bb5e0017b1a8d6" && (
-    //         <li>
-    //             <NavLink to="/search"> SEARCH ENGINE</NavLink>
-    //         </li>
-    //     )}
-    //     {(auth.isLoggedIn || !auth.isLoggedIn) && auth.userId !== "5ff23a8f60bb5e0017b1a8d6" && (
-    //         <li>
-    //             <NavLink to="/allhotels"> All HOTELS</NavLink>
-    //         </li>
-    //     )}
-    //     {!auth.isLoggedIn && (
-    //         <li>
-    //             <NavLink to="/auth"> AUTHENTICATE</NavLink>
-    //         </li>
-    //     )}
-    //     {auth.isLoggedIn && auth.userId === "5ff23a8f60bb5e0017b1a8d6" && (
-    //         <li>
-    //             <NavLink to={`/${auth.userId}/hotels`}> MY HOTELS</NavLink>
-    //         </li>
-    //     )}
-    //     {auth.isLoggedIn && auth.userId === "5ff23a8f60bb5e0017b1a8d6" && (
-    //         <li>
-    //             <NavLink to="/hotels/new"> ADD HOTELS</NavLink>
-    //         </li>
-    //     )}
-    //     {auth.isLoggedIn && auth.userId === "5ff23a8f60bb5e0017b1a8d6" && (
-    //         <li>
-    //             <NavLink to="/hotels/users"> SEE ALL USERS</NavLink>
-    //         </li>
-    //     )}
-    //     {auth.isLoggedIn && (
-    //         <li>
-    //             <button onClick = {auth.logout}>LOGOUT</button>
-    //         </li>
-    //     )}
-    //
-    // </ul>
     );
 };
 
